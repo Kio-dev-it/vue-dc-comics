7332643e1baf2bb3ba29a85d8822fa99722abe2c
Kio-dev-it <pas.petito@gmail.com>
2021-10-01 09:09:54 +0200

    Add provisional height to Content component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
-        <h2>Content goes here</h2>
+        <div class="container">
+            <h2>Content goes here</h2>
+        </div>
     </main>
 </template>
 
@@ -11,12 +13,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+
 main{
     background-color: #000;
-
+    height: 120px; /*provisional*/
+    
     h2{
         color: #fff;
-        font-weight: 500;
+        font-weight: 700;
     }
 }
 
