a6b52c4dacda1edb912494ba8c6d1b9e4bc4b4dc
Kio-dev-it <pas.petito@gmail.com>
2021-10-01 23:46:49 +0200

    Add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header/>
+    <Hero :bgImg="require('./assets/images/jumbotron.jpg')"/>
     <Content/>
     <MegaIcons/>
     <Footer/>
@@ -9,6 +10,7 @@
 
 <script>
 import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
 import Content from './components/Content.vue'
 import MegaIcons from './components/MegaIcons.vue'
 import Footer from './components/Footer.vue'
@@ -18,6 +20,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Hero,
     Content,
     MegaIcons,
     Footer
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hero" :style="`background-image: url(${bgImg})`">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+    props: ['bgImg']
+
+}
+</script>
+
+<style lang="scss" scoped>
+    
+    .hero{
+        height: 25rem;
+        background-position: top;
+        background-size: cover;
+    }   
+
+</style>
