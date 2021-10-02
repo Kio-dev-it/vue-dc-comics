5dc17166224858e642675aede906f45884a8f3b4
Kio-dev-it <pas.petito@gmail.com>
2021-10-02 16:11:42 +0200

    Add comic content

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="card">
+        <div class="card__image">
+            <div class="image-container">
+                <img :src="comicInfo.thumb" :alt="comicInfo.series" class="comic-cover">
+            </div>
+        </div>
+        <div class="card__title">
+            <h3 class="comic-title">{{comicInfo.series}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        comicInfo: Object
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .card{
+        margin: 0 .9375rem 3.125rem .9375rem;
+
+        width: calc( 100% / 6 - 30px );
+
+        .card__image{
+            margin-bottom: 1.25rem;
+
+            .image-container{
+                width: 100%;
+                padding-bottom: 100%;
+                position: relative;
+            }
+            img{
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                object-fit: cover;
+                object-position: top;
+            }
+        }
+
+        .card__title{
+            font-weight: 700;
+            color: #fff;
+            font-size: .6875rem;
+
+            .comic-title{
+                text-transform: uppercase;
+            }
+        }
+    }
+
+
+</style>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,28 +1,69 @@
 <template>
     <main>
         <div class="container">
-            <h2>Content goes here</h2>
+            <div class="collection-type">
+                CURRENT SERIES
+            </div>
+            <ComicCard v-for="(elm, index) in comicInfo" :key="index" :comicInfo="elm"/>
         </div>
+        <a href="#" class="load">LOAD MORE</a>
     </main>
 </template>
 
 <script>
+import ComicCard from './ComicCard.vue';
+import comics from '../assets/data/dc-comics.json'
+
 export default {
-    name: 'Content'
+    name: 'Content',
+    components: {
+        ComicCard
+    },
+    data(){
+        return {
+            comicInfo: comics,
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/style/variables.scss';
 
-main{
-    background-color: #000;
-    height: 120px; /*provisional*/
-    
-    h2{
+    main{
+        background-color: #000;
+        padding: 4.0625rem 0 1rem 0; 
         color: #fff;
-        font-weight: 700;
+
+        .container{
+            display: flex;
+            flex-wrap: wrap;
+            position: relative;
+
+            .collection-type{
+                position: absolute;
+                left: 0;
+                top: -4.0625rem;
+                transform: translateY(-50%);
+                background-color: $mainColor;
+                padding: .5rem 1rem;
+                font-weight: 700;
+                font-size: 1.5rem;
+            }
+        }
+
+        .load{
+            display: inline-block;
+            text-decoration: none;
+            padding: .5rem 1.5rem;
+            background-color: $mainColor;
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
+            font-weight: 700;
+            color: #fff;
+        }
+        
     }
-}
 
 </style>
